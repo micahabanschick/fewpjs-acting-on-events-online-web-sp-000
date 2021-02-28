5eb5f2d8ca9d4a417f31c72645474c5db7fa61be
Micah Banschick <micha.ban@gmail.com>
2021-02-28 22:07:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ function moveDodgerRight() {
   let right = parseInt(rightNumbers, 10);
 
   if (right > 0) {
-    dodger.style.right = `${right - 1}px`;
+    dodger.style.right = `${right + 1}px`;
   }
 }
