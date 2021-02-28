8569f8bb2cbecbd2175c60c241c0aae47ae5098b
Micah Banschick <micha.ban@gmail.com>
2021-02-28 22:01:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 // Your code here
 let dodger = document.getElementById("dodger");
 dodger.style.backgroundColor = "#FF69B4";
+console.log(dodger.style.left); // "180px"
+console.log(dodger.style.bottom); // "0px"
+dodger.style.bottom = "100px";
+dodger.style.bottom = "0px";
