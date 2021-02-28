17728a07cd40bdb56c0a7b5880e2caf8c31b4b65
Micah Banschick <micha.ban@gmail.com>
2021-02-28 22:07:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 // Your code here
 let dodger = document.getElementById("dodger");
-dodger.style.backgroundColor = "#FF69B4";
-console.log(dodger.style.left); // "180px"
-console.log(dodger.style.bottom); // "0px"
-dodger.style.bottom = "100px";
-dodger.style.bottom = "0px";
+
 
 function moveDodgerLeft() {
   let leftNumbers = dodger.style.left.replace("px", "");
@@ -20,6 +16,6 @@ function moveDodgerRight() {
   let right = parseInt(rightNumbers, 10);
 
   if (right > 0) {
-    dodger.style.right = `${right}px`;
+    dodger.style.right = `${right - 1}px`;
   }
 }
